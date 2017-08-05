374a019a425d76a5d8adb042d851e8dc5a38abeb
pfc <peoplesfeelingscode@gmail.com>
2017-08-05 07:21:50 -0600

    implemented dial

--- app/src/main/java/peoplesfeelingscode/com/samplemetronomerebuild/ActivityMain.java
+++ app/src/main/java/peoplesfeelingscode/com/samplemetronomerebuild/ActivityMain.java
@@ -1,7 +1,10 @@
 package peoplesfeelingscode.com.samplemetronomerebuild;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.WarwickWestonWright.HGDialV2.HGDialInfo;
+import com.WarwickWestonWright.HGDialV2.HGDialV2;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HGDialV2 hgDialV2 = (HGDialV2) findViewById(R.id.hgDialV2);
+
+        HGDialV2.IHGDial ihgDial = new HGDialV2.IHGDial() {
+            @Override
+            public void onDown(HGDialInfo hgDialInfo) { /* Do Your Thing */ }
+            @Override
+            public void onPointerDown(HGDialInfo hgDialInfo) { /* Do Your Thing */ }
+            @Override
+            public void onMove(HGDialInfo hgDialInfo) { /* Do Your Thing */ }
+            @Override
+            public void onPointerUp(HGDialInfo hgDialInfo) { /* Do Your Thing */ }
+            @Override
+            public void onUp(HGDialInfo hgDialInfo) { /* Do Your Thing */ }
+        };
+
+        hgDialV2.registerCallback(ihgDial);
+
     }
 }
