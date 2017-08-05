c217c24286cbc2b6a9f57f31fb7b49f8b113a8d8
pfc <peoplesfeelingscode@gmail.com>
2017-08-05 09:07:49 -0600

    comments

--- app/src/main/java/peoplesfeelingscode/com/samplemetronomerebuild/ObjectFile.java
+++ app/src/main/java/peoplesfeelingscode/com/samplemetronomerebuild/ObjectFile.java
@@ -2,10 +2,6 @@ package peoplesfeelingscode.com.samplemetronomerebuild;
 
 import java.io.Serializable;
 
-/**
- * Created by User Name on 10/3/2016.
- */
-
 public class ObjectFile implements Serializable {
     String name;
     boolean selected;
