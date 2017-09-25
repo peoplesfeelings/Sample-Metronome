c3578c997ae1510027ce9bab9fbbab77883a48f8
pfc <peoplesfeelingscode@gmail.com>
2017-09-24 22:21:00 -0400

    (activity main) code organization. moved a couple methods and removed some methods used only for log prints

--- app/src/main/java/peoplesfeelingscode/com/samplemetronomerebuild/ActivityMain.java
+++ app/src/main/java/peoplesfeelingscode/com/samplemetronomerebuild/ActivityMain.java
@@ -35,7 +35,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -121,41 +120,45 @@ public class ActivityMain extends ActivityBase {
     }
 
     @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("***************", "onRestart()");
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
 
         loopRunning = false;
-        Log.d("***************", "onDestroy()");
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("***************", "onStart()");
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE_FOR_IMPORT: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Storage.writeSamplePack(this);
+                    loadFile(Storage.getSharedPrefString(Storage.SHARED_PREF_SELECTED_FILE_KEY, this));
+                } else {
+                    Toast toast = new Toast(this);
+                    toast.setView(getLayoutInflater().inflate(R.layout.toast, null));
+                    toast.setDuration(Toast.LENGTH_LONG);
+                    TextView text = (TextView) toast.getView().findViewById(R.id.txt);
+                    text.setText(getResources().getString(R.string.toastWritePermissionNotGranted));
+                    toast.show();
+
+                    finish();
+                }
+                return;
+            }
+        }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("***************", "onStop()");
-    }
+    private void getPermissionForWrite() {
+        permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("***************", "onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("***************", "onPause()");
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE_FOR_IMPORT);
+        } else {
+            //
+        }
     }
 
     protected void createSoundPool() {
@@ -406,40 +409,5 @@ public class ActivityMain extends ActivityBase {
             Storage.writeSamplePack(this);
         }
 
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE_FOR_IMPORT: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Storage.writeSamplePack(this);
-                    loadFile(Storage.getSharedPrefString(Storage.SHARED_PREF_SELECTED_FILE_KEY, this));
-                } else {
-                    Toast toast = new Toast(this);
-                    toast.setView(getLayoutInflater().inflate(R.layout.toast, null));
-                    toast.setDuration(Toast.LENGTH_LONG);
-                    TextView text = (TextView) toast.getView().findViewById(R.id.txt);
-                    text.setText(getResources().getString(R.string.toastWritePermissionNotGranted));
-                    toast.show();
-
-                    finish();
-                }
-                return;
-            }
-        }
-    }
-
-    private void getPermissionForWrite() {
-        permissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE_FOR_IMPORT);
-        } else {
-            //
-        }
     }
 }
