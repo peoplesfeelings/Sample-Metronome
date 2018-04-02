a7a3634b9d528c8ee5e123d6574fcccb86c863fc
pfc <peoplesfeelingscode@gmail.com>
2018-04-02 00:05:03 -0400

    (service) added api-conditional notification channel for oreo

--- app/src/main/java/peoplesfeelingscode/com/samplemetronomerebuild/MyService.java
+++ app/src/main/java/peoplesfeelingscode/com/samplemetronomerebuild/MyService.java
@@ -1,6 +1,8 @@
 package peoplesfeelingscode.com.samplemetronomerebuild;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
@@ -125,15 +127,35 @@ public class MyService extends Service {
         PendingIntent pendingIntent =
                 PendingIntent.getActivity(context, 0, intent, 0);
 
-        notification =
-                new Notification.Builder(context)
-                        .setPriority(Notification.PRIORITY_HIGH)
-                        .setContentTitle(getText(R.string.notifTitle))
-                        .setContentText(getText(R.string.notifText))
-                        .setSmallIcon(R.drawable.notif_icon)
-                        .setContentIntent(pendingIntent)
-                        .setTicker(getText(R.string.notifTicker))
-                        .build();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
+            String channelId = "the sample id";
+            CharSequence name = getString(R.string.notif_channel_name);// The user-visible name of the channel.
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(channelId, name, importance);
+
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.createNotificationChannel(mChannel);
+
+            notification =
+                    new Notification.Builder(context, channelId)
+                            .setContentTitle(getText(R.string.notifTitle))
+                            .setContentText(getText(R.string.notifText))
+                            .setSmallIcon(R.drawable.notif_icon)
+                            .setContentIntent(pendingIntent)
+                            .setTicker(getText(R.string.notifTicker))
+                            .build();
+        } else {
+            notification =
+                    new Notification.Builder(context)
+                            .setPriority(Notification.PRIORITY_HIGH)
+                            .setContentTitle(getText(R.string.notifTitle))
+                            .setContentText(getText(R.string.notifText))
+                            .setSmallIcon(R.drawable.notif_icon)
+                            .setContentIntent(pendingIntent)
+                            .setTicker(getText(R.string.notifTicker))
+                            .build();
+        }
     }
 
     void loadFile(String fileName) {
